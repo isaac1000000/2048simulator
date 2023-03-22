60acc1b79bdb5c87e493817c41c1fbcc8d940974
isaac1000000 <isaac.fisher@nyu.edu>
2023-03-21 20:56:29 -0400

    Create RandomMoves.java

---
+++ src/sim/RandomMoves.java
@@ -0,0 +1,25 @@
+package sim;
+
+import sim.game.Mover;
+import java.util.Random;
+
+public class RandomMoves extends TestCase{
+	Random rand = new Random();
+
+	public RandomMoves(){
+		name = "Random Moves";
+	}
+
+	// Cycles through left, up, right
+	@Override
+	public int makeDecision(Mover mover) {
+		return rand.nextInt(4);
+	}
+	
+	public static void main(String[] args) {
+		LeftUpRightDown test = new LeftUpRightDown();
+		test.runTests();
+		System.out.println(test.resultsString());
+		System.out.println(test.maxBoardString());
+	}
+}
