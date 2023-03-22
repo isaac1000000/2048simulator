344187248a7d80522e1e15f7879bf4d2e03684bd
isaac1000000 <isaac.fisher@nyu.edu>
2023-03-21 21:42:50 -0400

    Got rid of redundancy and added a double circle

--- src/sim/LeftUpRightDown2.java
+++ src/sim/LeftUpRightDown2.java
@@ -2,25 +2,25 @@ package sim;
 
 import sim.game.Mover;
 
-public class LeftUpRightDown extends TestCase{
-	int[] current_move = {2, 0, 3, 1};
+public class LeftUpRightDown2 extends TestCase{
+	int[] current_move = {2, 2, 0, 0, 3, 3, 1, 1};
 	int index = 0;
 	
-	public LeftUpRightDown(){
-		name = "Left, Up, Right, Down";
+	public LeftUpRightDown2(){
+		name = "Left, Left, Up, Up, Right, Right, Down, Down";
 	}
 
 	// Cycles through left, up, right, down
 	@Override
 	public int makeDecision(Mover mover) {
-		if (index > 3) {
-			index -= 4;
+		if (index > 7) {
+			index -= 8;
 		}
 		return current_move[index++];
 	}
-	
+
 	public static void main(String[] args) {
-		LeftUpRightDown test = new LeftUpRightDown();
+		LeftUpRightDown2 test = new LeftUpRightDown2();
 		test.runTests();
 		System.out.println(test.resultsString());
 		System.out.println(test.maxBoardString());
